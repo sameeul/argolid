6ed21fc7586a4d6012fb00d49b90324bb415a593
Sameeul Bashir Samee <sameeul@gmail.com>
2024-09-13 22:55:57 -0400

    Pyramid composition (#17)
    On demand chunk generation

--- src/python/argolid/__init__.py
+++ src/python/argolid/__init__.py
@@ -1,4 +1,4 @@
 from .pyramid_generator import PyramidGenerartor, PyramidView, PlateVisualizationMetadata, Downsample
-
+from .pyramid_compositor import PyramidCompositor
 from . import _version
 __version__ = _version.get_versions()['version']
