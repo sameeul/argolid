e71a9abd3ddc6f61c1e83207615d8416919b62ca
Sameeul Samee <sameeul@gmail.com>
2024-10-07 06:22:44 -0400

    WIP 6 Volume generator

--- src/python/argolid/__init__.py
+++ src/python/argolid/__init__.py
@@ -1,5 +1,11 @@
-from .pyramid_generator import PyramidGenerartor, PyramidView, PlateVisualizationMetadata, Downsample
+from .pyramid_generator import (
+    PyramidGenerartor,
+    PyramidView,
+    PlateVisualizationMetadata,
+    Downsample,
+)
 from .volume_generator import VolumeGenerator, PyramidGenerator3D
 
 from . import _version
-__version__ = _version.get_versions()['version']
+
+__version__ = _version.get_versions()["version"]
--- src/python/argolid/volume_generator.py
+++ src/python/argolid/volume_generator.py
@@ -6,19 +6,47 @@ import numpy as np
 import concurrent.futures
 from multiprocessing import get_context
 import json
-
+from typing import List, Tuple
 
 CHUNK_SIZE = 1024
 
 
-
 class VolumeGenerator:
-    def __init__(self, source_dir, group_by, file_pattern, out_dir, out_name) -> None:
-        self._source_dir = source_dir
-        self._group_by = group_by
-        self._file_pattern = file_pattern
-        self._out_dir = out_dir
-        self._out_name = out_name
+    """
+    A class for generating volumetric data from image files and storing it in a Zarr array.
+
+    This class handles the process of reading image files, grouping them based on specified criteria,
+    and writing the data into a Zarr array for efficient storage and access of large-scale volumetric data.
+
+    Attributes:
+        _source_dir (str): Directory containing the source image files.
+        _group_by (str): Criterion for grouping images ('c', 't', or 'z').
+        _file_pattern (str): Pattern to match the image files.
+        _out_dir (str): Output directory for the generated Zarr array.
+        _out_name (str): Name of the output Zarr array.
+        _X (int): Width of the images.
+        _Y (int): Height of the images.
+        files (List[str]): List of image file paths.
+        _zarr_spec (dict): Specification for the Zarr array.
+    """
+
+    def __init__(
+        self,
+        source_dir: str,
+        group_by: str,
+        file_pattern: str,
+        out_dir: str,
+        out_name: str,
+    ) -> None:
+        self._source_dir: str = source_dir
+        self._group_by: str = group_by
+        self._file_pattern: str = file_pattern
+        self._out_dir: str = out_dir
+        self._out_name: str = out_name
+        self._X: int
+        self._Y: int
+        self.files: List[str]
+        self._zarr_spec: dict
 
     def init_base_zarr_file(self):
         # find out what are the dimension of the zarr array
@@ -39,79 +67,96 @@ class VolumeGenerator:
             self._Y = br.Y
             dtype = br.dtype
 
-        z_size = 1
-        t_size = 1
-        c_size = 1
+        self._Z = 1
+        self._T = 1
+        self._C = 1
 
         if self._group_by == "c":
-            c_size = dim_size
+            self._C = dim_size
         elif self._group_by == "t":
-            t_size = dim_size
+            self._T = dim_size
         elif self._group_by == "z":
-            z_size = dim_size
+            self._Z = dim_size
         else:
             pass
 
         self._zarr_spec = {
-            "driver":"zarr",
-            "kvstore":{
-                "driver":"file",
-                "path" : f"{self._out_dir}/{self._out_name}/0",
+            "driver": "zarr",
+            "kvstore": {
+                "driver": "file",
+                "path": f"{self._out_dir}/{self._out_name}/0",
             },
             "create": True,
             "delete_existing": False,
             "open": True,
-            "metadata" : {
-                "shape" : [c_size, z_size, self._Y, self._X],
-                "chunks" : [1,1,CHUNK_SIZE,CHUNK_SIZE],
-                "dtype" : np.dtype(dtype).str,
-                "dimension_separator" : "/",
-                "compressor" : {"id": "blosc", "cname": "zstd", "clevel": 1, "shuffle": 1, "blocksize": 0},
+            "metadata": {
+                "shape": [self._C, self._Z, self._Y, self._X],
+                "chunks": [1, 1, CHUNK_SIZE, CHUNK_SIZE],
+                "dtype": np.dtype(dtype).str,
+                "dimension_separator": "/",
+                "compressor": {
+                    "id": "blosc",
+                    "cname": "zstd",
+                    "clevel": 1,
+                    "shuffle": 1,
+                    "blocksize": 0,
                 },
-            'context': {
-                'cache_pool': {},
-                'data_copy_concurrency': {"limit": os.cpu_count()//2},
-                'file_io_concurrency': {"limit": os.cpu_count()//2},
-                'file_io_sync': False,
-                },
-    
-            }
+            },
+            "context": {
+                "cache_pool": {},
+                "data_copy_concurrency": {"limit": os.cpu_count() // 2},
+                "file_io_concurrency": {"limit": os.cpu_count() // 2},
+                "file_io_sync": False,
+            },
+        }
 
+    def layer_writer(self, args: Tuple[str, dict, int, int]) -> None:
+        """
+        Write a single layer of the volume to the Zarr array.
 
+        Args:
+            args (Tuple[str, dict, int, int]): Tuple containing input file path, Zarr specification, z-index, and c-index.
+        """
+        input_file: str = args[0]
+        zarr_spec: dict = args[1]
+        z: int = args[2]
+        c: int = args[3]
 
-    def layer_writer(self, args):
-        input_file = args[0] 
-        zarr_spec = args[1]
-        z = args[2] 
-        c = args[3] 
-
-        zarr_array = ts.open(zarr_spec).result()
-        write_futures = []
+        zarr_array: ts.TensorStore = ts.open(zarr_spec).result()
+        write_futures: List[ts.Future[None]] = []
         try:
-            br = BioReader(input_file, backend="tensorstore")
-            print(input_file)
+            br: BioReader = BioReader(input_file, backend="tensorstore")
             for y in range(0, br.Y, CHUNK_SIZE):
-                y_max = min([br.Y, y + CHUNK_SIZE])
+                y_max: int = min([br.Y, y + CHUNK_SIZE])
                 for x in range(0, br.X, CHUNK_SIZE):
-                    x_max = min([br.X, x + CHUNK_SIZE])
-                    write_futures.append(zarr_array[c,z,y:y_max, x:x_max].write(br[y:y_max, x:x_max,0,0,0].squeeze()))
+                    x_max: int = min([br.X, x + CHUNK_SIZE])
+                    write_futures.append(
+                        zarr_array[c, z, y:y_max, x:x_max].write(
+                            br[y:y_max, x:x_max, 0, 0, 0].squeeze()
+                        )
+                    )
 
             for future in write_futures:
-                    future.result()
+                future.result()
         except Exception as e:
             print(f"Caught an exception for item : {e}")
 
-
     def generate_volume(self):
+        """
+        Generate the complete volume by initializing the Zarr file and writing the image stack.
+        """
         self.init_base_zarr_file()
         self.write_image_stack()
 
     def write_image_stack(self):
+        """
+        Write the entire image stack to the Zarr array using parallel processing.
+        """
         count = 0
         arg_list = []
         for file_name in self.files:
             t = 0
-            c = 0   
+            c = 0
             z = 0
             if self._group_by == "c":
                 c = count
@@ -119,13 +164,23 @@ class VolumeGenerator:
                 c = count
             elif self._group_by == "z":
                 z = count
-            
+
             arg_list.append((file_name, self._zarr_spec, z, c, t))
             count += 1
-        with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count()//2, mp_context=get_context("spawn")) as executor:
-            executor.map(self.layer_writer, arg_list) 
+        with concurrent.futures.ProcessPoolExecutor(
+            max_workers=os.cpu_count() // 2, mp_context=get_context("spawn")
+        ) as executor:
+            executor.map(self.layer_writer, arg_list)
 
 
+    def generate_downsampled_images(self):
+        min_dim = min(self._X, self._Y, self._Z)
+        max_level = int(np.log2(min_dim))
+        min_level = int(np.log2(512))
+        base_level = 0
+        pyr_gen = PyramidGenerator3D(f"{self._out_dir}/{self._out_name}",str(base_level))
+        for level in range(base_level, base_level+max_level-min_level):
+            self.downsample_pyramid(level)
 
 
 
@@ -134,69 +189,44 @@ class PyramidGenerator3D:
         self._zarr_loc_dir = zarr_loc_dir
         self._base_level = base_level
 
-    def calculate_downsampling_factors(self, dimensions, downsampling_steps):
-        """
-        Calculate downsampling factors to make the final voxels approximately cubic.
-        
-        Args:
-            dimensions (tuple): A tuple containing the original dimensions (L_x, L_y, L_z).
-            downsampling_steps (int): The number of times to downsample along each axis.
-
-        Returns:
-            list: A list of downsampling factors along each axis (r_x, r_y, r_z).
-        """
-        L_x, L_y, L_z = dimensions
-
-        # Calculate the geometric mean of the cuboid dimensions
-        geometric_mean = (L_x * L_y * L_z) ** (1 / 3)
-
-        # Calculate initial downsampling factors based on the geometric mean
-        r_x = L_x / geometric_mean
-        r_y = L_y / geometric_mean
-        r_z = L_z / geometric_mean
-
-        # Normalize the factors and adjust them to fit the downsampling steps
-        downsampling_factors = [
-            np.round(r_x ** (1 / downsampling_steps)),
-            np.round(r_y ** (1 / downsampling_steps)),
-            np.round(r_z ** (1 / downsampling_steps)),
-        ]
-        
-        return downsampling_factors
-
-
     def downsample_pyramid(self, level):
 
         ds_spec = {
-                    "driver": "downsample",
-                    "downsample_factors": [1,2**level, 2**level, 2**level],
-                    "downsample_method": "mean",
-                    "base": {
-                                "driver": "zarr", 
-                                "kvstore": {            
-                                    "driver":"file",
-                                    "path" : f"{self._zarr_loc_dir}/{self._base_level}"
-                                }
-                            }
-                }
+            "driver": "downsample",
+            "downsample_factors": [1, 2**level, 2**level, 2**level],
+            "downsample_method": "mean",
+            "base": {
+                "driver": "zarr",
+                "kvstore": {
+                    "driver": "file",
+                    "path": f"{self._zarr_loc_dir}/{self._base_level}",
+                },
+            },
+        }
 
         ds_zarr_array = ts.open(ds_spec).result()
         [C, Z, Y, X] = ds_zarr_array.shape
         ds_write_spec = {
-            "driver":"zarr",
-            "kvstore":{
-                "driver":"file",
-                "path" : f"{self._zarr_loc_dir}/{level}",
+            "driver": "zarr",
+            "kvstore": {
+                "driver": "file",
+                "path": f"{self._zarr_loc_dir}/{level}",
             },
             "create": True,
             "delete_existing": True,
-            "metadata" : {
-                "shape" : ds_zarr_array.shape,
-                "chunks" : [1,1,CHUNK_SIZE,CHUNK_SIZE],
-                "dtype" : np.dtype(ds_zarr_array.dtype.numpy_dtype).str,
-                "dimension_separator" : "/",
-                "compressor" : {"id": "blosc", "cname": "zstd", "clevel": 1, "shuffle": 1, "blocksize": 0},
-                }       
+            "metadata": {
+                "shape": ds_zarr_array.shape,
+                "chunks": [1, 1, CHUNK_SIZE, CHUNK_SIZE],
+                "dtype": np.dtype(ds_zarr_array.dtype.numpy_dtype).str,
+                "dimension_separator": "/",
+                "compressor": {
+                    "id": "blosc",
+                    "cname": "zstd",
+                    "clevel": 1,
+                    "shuffle": 1,
+                    "blocksize": 0,
+                },
+            },
         }
 
         ds_zarr_array_write = ts.open(ds_write_spec).result()
@@ -207,7 +237,11 @@ class PyramidGenerator3D:
                     y_max = min([Y, y + CHUNK_SIZE])
                     for x in range(0, X, CHUNK_SIZE):
                         x_max = min([X, x + CHUNK_SIZE])
-                        write_futures.append(ds_zarr_array_write[c, z, y:y_max, x:x_max].write(ds_zarr_array[c, z, y:y_max, x:x_max].read().result()))
+                        write_futures.append(
+                            ds_zarr_array_write[c, z, y:y_max, x:x_max].write(
+                                ds_zarr_array[c, z, y:y_max, x:x_max].read().result()
+                            )
+                        )
 
         for future in write_futures:
             future.result()
@@ -219,43 +253,20 @@ class PyramidGenerator3D:
         attr_dict: dict = {}
 
         axes_metadata = [
-                            {
-                                "name": "c",
-                                "type": "channel"
-                            },
-                            {
-                                "name": "z",
-                                "type": "space",
-                                "unit": "micrometer"
-                            },
-                            {
-                                "name": "y",
-                                "type": "space",
-                                "unit": "micrometer"
-                            },
-                            {
-                                "name": "x",
-                                "type": "space",
-                                "unit": "micrometer"
-                            }
-                        ]
+            {"name": "c", "type": "channel"},
+            {"name": "z", "type": "space", "unit": "micrometer"},
+            {"name": "y", "type": "space", "unit": "micrometer"},
+            {"name": "x", "type": "space", "unit": "micrometer"},
+        ]
         attr_dict["axes"] = axes_metadata
         multiscale_metadata: list = []
         for key in range(min_level, min_level + num_levels):
-            metadata =  {
-                            "coordinateTransformations": [
-                                {
-                                    "scale": [
-                                        1,
-                                        2**key,
-                                        2**key,
-                                        2**key
-                                    ],
-                                    "type": "scale"
-                                }
-                            ],
-                            "path": f"{key}"
-                        }
+            metadata = {
+                "coordinateTransformations": [
+                    {"scale": [1, 2**key, 2**key, 2**key], "type": "scale"}
+                ],
+                "path": f"{key}",
+            }
             multiscale_metadata.append(metadata)
 
         attr_dict["datasets"] = multiscale_metadata
@@ -268,8 +279,11 @@ class PyramidGenerator3D:
         with open(f"{self._zarr_loc_dir}/.zattrs", "w") as json_file:
             json.dump(final_attr_dict, json_file)
 
-    def generate_pyramid(self):
-        num_levels = 6 # for now
+    def generate_pyramid(self, start_level, end_level):
+
+        num_levels = end_level-start_level+1
         self._create_zattr_file(self._base_level, num_levels)
-        with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count()//2, mp_context=get_context("spawn")) as executor:
-            executor.map(self.downsample_pyramid, range(num_levels)) 
+        with concurrent.futures.ProcessPoolExecutor(
+            max_workers=os.cpu_count() // 2, mp_context=get_context("spawn")
+        ) as executor:
+            executor.map(self.downsample_pyramid, range(num_levels))
